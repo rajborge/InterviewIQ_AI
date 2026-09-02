0541348c7cb5380fcab646ce3aac2667b33a82d2
RAJ RAVINDRA BORGE <rajborge3085@gmail.com>
2026-09-02 21:16:28 +0530

    Initial Project Commit

---
+++ backend/app/database/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from ..core.config import settings
+
+engine=create_engine(
+    settings.database_url,
+) 
+
+SessionLocal=sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
